d8e5b4a8aa6be50b3dd9ff508691aeb935925299
meng <monkeystylle@gmail.com>
2022-08-24 10:10:32 +0800

    added tweaks on animation for navbar modal

--- components/NavbarMenuModal/NavbarMenuModal.tsx
+++ components/NavbarMenuModal/NavbarMenuModal.tsx
@@ -3,7 +3,6 @@ import { DialogOverlay, DialogContent } from '@reach/dialog';
 import styled from 'styled-components';
 import { keyframes } from 'styled-components';
 import { X } from 'react-feather';
-import Hamburger from 'hamburger-react';
 
 interface ImenuModal {
   isOpen: boolean;
@@ -12,31 +11,53 @@ interface ImenuModal {
 
 const NavbarMenuModal = ({ isOpen, onDismiss }: ImenuModal) => {
   return (
-    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+    <Wrapper isOpen={isOpen} onDismiss={onDismiss}>
+      <Backdrop />
       <Content>
-        <CloseButtonWrapper onClick={onDismiss}>
-          <X size={35} />
-        </CloseButtonWrapper>
-        <Nav>
-          <NavLink href="/sale">Home</NavLink>
-          <NavLink href="/new">About Us</NavLink>
-          <NavLink href="/men">Portfolio</NavLink>
-          <NavLink href="/women">Contact</NavLink>
-        </Nav>
+        <InnerWrapper>
+          <CloseButtonWrapper onClick={onDismiss}>
+            <X size={35} />
+          </CloseButtonWrapper>
+          <Nav>
+            <NavLink href="/sale">Home</NavLink>
+            <NavLink href="/new">About Us</NavLink>
+            <NavLink href="/men">Portfolio</NavLink>
+            <NavLink href="/women">Contact</NavLink>
+          </Nav>
+        </InnerWrapper>
       </Content>
-    </Overlay>
+    </Wrapper>
   );
 };
 
 //MODAL
-const Overlay = styled(DialogOverlay)`
-  position: fixed;
+
+const fadeIn = keyframes`
+  from {
+    opacity:0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   background: hsl(220deg 5% 40% / 0.8);
+  animation: ${fadeIn} 500ms ease;
+`;
 
+const Wrapper = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: transparent;
   display: flex;
   justify-content: flex-end;
 `;
@@ -51,15 +72,25 @@ const slideInRight = keyframes`
 `;
 
 const Content = styled(DialogContent)`
+  position: relative;
   background: white;
   width: 300px;
   height: 100%;
-  padding: 32px;
+
+  animation: ${slideInRight} 500ms both cubic-bezier(0, 0.6, 0.32, 1.06);
+  animation-delay: 200ms;
+`;
+
+const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  padding: 32px;
+
   justify-content: center;
 
-  animation: ${slideInRight} 400ms ease;
+  animation: ${fadeIn} 600ms both ease-out;
+  animation-delay: 450ms;
 `;
 
 const CloseButtonWrapper = styled.button`
